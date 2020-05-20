e8b8226241ce882b044228e25b602c2fa20d02af
RishabhB99 <46969858+RishabhB99@users.noreply.github.com>
2020-05-20 18:54:13 +0530

    Add files via upload

--- detector.py
+++ detector.py
@@ -1,95 +1,22 @@
-import cld3
-import langid
-import csv
-from langdetect import detect
-import fasttext
 import sys
-import re
+from utilities import *
 
 
-#languages supported by all detectors
-language_set = {"af", "ar", "bg", "bn", "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "fa", "fi", "fr", "gu", "he",
-"hi", "hr", "hu", "id", "it", "ja", "kn", "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl",
-"pt", "ro", "ru", "sk", "sl", "so", "sq", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "ur", "vi", "zh-cn", "zh-tw"}
-
-
-def cld3accuracy(input_set,output_set,dataset_size,check):
+def accuracy(input_set,output_set,dataset_size,check,detector):
 	correct=0
 	total=0.0
 	for itr in range(dataset_size):
 		if(check):
 			if(output_set[itr] in language_set):
-				if(cld3.get_language(input_set[itr])[0]==output_set[itr]):
-					correct+=1
-				total+=1
-		else:		
-			if(cld3.get_language(input_set[itr])[0]==output_set[itr]):
-				correct+=1
-			total+=1		
-
-	return correct/total		
-
-
-def langidaccuracy(input_set,output_set,dataset_size,check):
-	correct=0
-	total=0.0
-	for itr in range(dataset_size):
-		if(check):
-			if(output_set[itr] in language_set):	
-				if(langid.classify(input_set[itr])[0]==output_set[itr]):
-					correct+=1
-				total+=1
-		else:	
-			if(langid.classify(input_set[itr])[0]==output_set[itr]):
-				correct+=1
-			total+=1
-						
-	return correct/total
-
-
-def langdetectaccuracy(input_set,output_set,dataset_size,check):
-	correct=0
-	total=0.0
-	for itr in range(dataset_size):
-		if(check):
-			if(output_set[itr] in language_set):	
-				if(detect(input_set[itr])==output_set[itr]):
-					correct+=1
-				total+=1
-		else:	
-			if(detect(input_set[itr])==output_set[itr]):
-				correct+=1
-			total+=1
-
-	return correct/total
-
-
-def fasttextaccuracy(input_set,output_set,dataset_size,check,model):
-	correct=0
-	total=0.0
-	for itr in range(dataset_size):
-		if(check):
-			if(output_set[itr] in language_set):
-				lb,acc=model.predict(input_set[itr])
-				if(lb[0][9:]==output_set[itr]):
+				if(detector.detect(input_set[itr])==output_set[itr]):
 					correct+=1
 				total+=1
 		else:
-			lb,acc=model.predict(input_set[itr])
-			if(lb[0][9:]==output_set[itr]):
+			if(detector.detect(input_set[itr])==output_set[itr]):
 				correct+=1
 			total+=1
 
 	return correct/total	
-
-def clean(text):
-	text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-	result = ''.join(i for i in text if not i.isdigit() )
-	result = result.replace('"', '')
-	result = result.replace('\t',' ')
-	result = result.replace('\n', '')
-
-	return result
 
 def command(argument):
 	global input_set,output_set,dataset_size
@@ -110,8 +37,8 @@ def command(argument):
 			input_set = inp.readlines()
 
 		for i in range(len(input_set)):
-			input_set[i]=clean(input_set[i])
-
+			input_set[i]=input_set[i].rstrip("\n")
+				
 		check = 1 	
 
 	if(argument[:8]=="--output"):
@@ -129,20 +56,21 @@ def command(argument):
 		check=0	
 
 	if(argument=="--cld3"):
-		print("CLD3 accuracy = ",cld3accuracy(input_set,output_set,dataset_size,check))	
+		print("CLD3 accuracy = ",accuracy(input_set,output_set,dataset_size,check,CLD3))	
 
 	if(argument=="--langid"):
-		print("LANGID accuracy = ",langidaccuracy(input_set,output_set,dataset_size,check))
+		print("LANGID accuracy = ",accuracy(input_set,output_set,dataset_size,check,LANGID))	
 	
 	if(argument=="--langdetect"):
-		print("LANGDETECT accuracy = ",langdetectaccuracy(input_set,output_set,dataset_size,check))
+		print("LANGDETECT accuracy = ",accuracy(input_set,output_set,dataset_size,check,LANGDETECT))	
 
 	if(argument=="--fasttext"):
-		model = fasttext.load_model('lid.176.ftz') #loading fasttext model
-		print("FASTTEXT accuracy = ",fasttextaccuracy(input_set,output_set,dataset_size,check,model))	
+		print("FASTTEXT accuracy = ",accuracy(input_set,output_set,dataset_size,check,FASTTEXT))	
 
 
+def main():
+	for i in range(1,len(sys.argv)):
+		command(sys.argv[i])	
 
 
-for i in range(1,len(sys.argv)):
-	command(sys.argv[i])	
+main()
---
+++ utilities.py
@@ -0,0 +1,49 @@
+import cld3
+import langid
+from langdetect import detect
+import fasttext
+import re
+#languages supported by all detectors
+language_set = {"af", "ar", "bg", "bn", "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "fa", "fi", "fr", "gu", "he",
+"hi", "hr", "hu", "id", "it", "ja", "kn", "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl",
+"pt", "ro", "ru", "sk", "sl", "so", "sq", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "ur", "vi", "zh-cn", "zh-tw"}
+
+	
+def clean(text):
+	text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+	result = ''.join(i for i in text if not i.isdigit() )
+	result = result.replace('"', '')
+	result = result.replace('\t',' ')
+	result = result.replace('\n', '')
+
+	return result
+
+class CLD3():
+	def detect(text):
+		prediction, confidence, isreliable, proportion = cld3.get_language(text)
+		if(confidence >= 0.5):
+			return prediction
+		else:	 
+			return cld3.get_language(clean(text))[0]
+
+class LANGID():
+	def detect(text):
+		prediction, confidence = langid.classify(text)
+		if(confidence >= 0):
+			return prediction
+		else:	 
+			return langid.classify(clean(text))[0]		
+
+class LANGDETECT():
+	def detect(text):
+		return detect(text)		
+
+class FASTTEXT():
+	global model
+	model = fasttext.load_model('lid.176.ftz')
+	def detect(text): #loading fasttext model
+		prediction, confidence = model.predict(text)
+		if(confidence[0] >= 0.5):
+			return prediction[0][9:]
+		else:	
+			return model.predict(clean(text))[0][0][9:]
